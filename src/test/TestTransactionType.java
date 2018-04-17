a6d62df6866995d557b91e2814d85a1138516fdb
M-Sloan <murraysloan90@gmail.com>
2018-04-17 14:32:34 -0400

    Test case for Enum

--- src/test/TestTransactionType.java
+++ src/test/TestTransactionType.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import banking.TransactionType;
+
 class TestTransactionType {
+	
+	private static final int NUM_TYPES = 7;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTransactionType() {
+		assertEquals(NUM_TYPES, TransactionType.values().length);
 	}
 
 }
