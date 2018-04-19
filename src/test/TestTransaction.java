6c9f9b2a7d4fe256bfb1b6be1185f02f2f72ee89
M-Sloan <murraysloan90@gmail.com>
2018-04-19 14:21:49 -0400

    testValidTransaction

--- src/test/TestTransaction.java
+++ src/test/TestTransaction.java
@@ -2,53 +2,29 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import banking.Transaction;
+import banking.TransactionType;
 
 class TestTransaction {
 
+	String description = "Billy's College Fund";
+	double amount = 120.45;
+	Transaction transaction = new Transaction(TransactionType.DEPOSIT, amount, description);
+	
+	
 	@Test
-	void testHashCode() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testTransaction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetId() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetTimestamp() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetType() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetAmount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetDescription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEqualsObject() {
-		fail("Not yet implemented");
+	void testValidTransaction() {
+		
+		System.out.println("Inside testValidTransaction()");
+		
+		Assert.assertEquals(description, 
+				"^[A-Za-z.\\s_-]+$", "Invalid description.");
+		Assert.assertEquals(amount, 
+				"^\\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$", "Invalid amount.");
 	}
 
 	@Test
