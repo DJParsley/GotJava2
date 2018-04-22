31e638ae2ff54092754ddea1e651b16257cc19f7
Datnewnew <35675172+Datnewnew@users.noreply.github.com>
2018-04-21 23:27:11 -0400

    Savings Updated.
    Not done...

--- src/test/TestSavingsAccount.java
+++ src/test/TestSavingsAccount.java
@@ -4,27 +4,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestSavingsAccount {
+class TestSavingsAccount extends Account {
+static double APR = 5.0;
 
 	@Test
-	void testDeposit() {
-		fail("Not yet implemented");
+	void testDeposit(double amount) {
+amount = Account.amount;
+double depositAmount = 200.00;
+if (depositAmount > amount)
+{
+println("Amount Added");
+amount = amount + depositAmount;
+}
+else
+{
+println("Deposit Should Be More Than The Initial Account Balance");
+}
 	}
 
 	@Test
 	void testWithdraw() {
-		fail("Not yet implemented");
+
+double mainAccount = this.amount;
+double fee = 25.00;
+double withdrawAmount = 400.00;
+
+if (withdrawAmount > mainAccount)
+{
+println("Insufficient Funds...With Ya Broke Self... Accessing $25 fee...");
+mainAccount = mainAccount - fee;
+println("Now Your Balance Is $" + "mainAccount" );
+}
+else
+{
+mainAccount = mainAccount - transferAmount;
+println("Withdraw Complete. Balance Is $" + mainAccount);
+}
+
 	}
 
 	@Test
-	void testSavingsAccountCustomerDoubleString() {
-		fail("Not yet implemented");
-	}
+public testSavingsAccount (Customer client, double startBalance, String description)
+{
+client = "Ricky Bobby";
+startBalance = 500.00;
+description = "This Account Is For Rickys Flame Proof Clothing";
+}
 
 	@Test
-	void testSavingsAccountCustomerDoubleStringDouble() {
-		fail("Not yet implemented");
-	}
+	public testSavingsAccount (Customer client, double startBalance, String description, double intRate) {
+
+client = "Ricky Bobby";
+startBalance = 500.00;
+description = "This Account Is For Rickys Flame Proof Clothing";
+intRate = 6.0;
+}
 
 	@Test
 	void testAddInterestTransaction() {
